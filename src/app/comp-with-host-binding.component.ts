0c8ec3c808d2a70ef4c3c41cb92ef0edd70521e2
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-12-09 19:05:57 +0530

    main TS

---
+++ src/app/comp-with-host-binding.component.ts
@@ -0,0 +1,17 @@
+import { Component, HostBinding } from '@angular/core';
+
+@Component({
+  selector: 'comp-with-host-binding',
+  template: 'I am a component!',
+})
+export class CompWithHostBindingComponent {
+  @HostBinding('class.special')
+  isSpecial = false;
+
+  @HostBinding('style.color')
+  color = 'green';
+
+  @HostBinding('style.width')
+  width = '200px';
+
+}
