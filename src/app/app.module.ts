03910953bd055c64f39b1b2ac4ec27255c919835
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-12-09 19:05:49 +0530

    App Module TS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { CompWithHostBindingComponent } from './comp-with-host-binding.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompWithHostBindingComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
